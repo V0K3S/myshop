c4d03571d6d253f99b2011c4b2132616ae68eebc
V0K3S <129615215+V0K3S@users.noreply.github.com>
2023-07-29 22:35:41 -0400

    debugging

--- cart/cart.py
+++ cart/cart.py
@@ -12,7 +12,7 @@ class Cart(object):
         cart = self.session.get(settings.CART_SESSION_ID)
         if not cart:
             #save an empty cart in the session
-            cart = self.session[settings.CART_SESSION_ID]
+            cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
     def add(self, product, quantity=1, override_quantity=False):
         """
@@ -61,7 +61,7 @@ class Cart(object):
     
     def get_total_price(self):
         #get total price of items in cart
-        return sum(Decimal(item['price']) * [item['quantity']] for item in self.cart.value())
+        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
     
     def clear(self):
         #remove cart from session
--- cart/views.py
+++ cart/views.py
@@ -6,8 +6,8 @@ from .forms import CartAddProductForm
 
 @require_POST
 def cart_add(request, product_id):
-    cart = cart(request)
-    product = get_object_or_404(product, id=product_id)
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
     form = CartAddProductForm(request.POST)
     if form.is_valid():
         cd = form.cleaned_data
